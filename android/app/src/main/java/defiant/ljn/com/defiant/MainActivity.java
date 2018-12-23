327bde1ae37fd6c74c47c5e1f6bb52ac40bd510a
Bleach1 <963262676@qq.com>
2018-12-23 12:05:20 +0800

    Initial commit

---
+++ android/app/src/main/java/defiant/ljn/com/defiant/MainActivity.java
@@ -0,0 +1,13 @@
+package defiant.ljn.com.defiant;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
