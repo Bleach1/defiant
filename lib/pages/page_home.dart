b9f14ee54ec891df38685bbd5fcd4b8e4cc2fcd8
Bleach1 <963262676@qq.com>
2019-01-20 12:11:51 +0800

    sssss

--- lib/pages/page_home.dart
+++ lib/pages/page_home.dart
@@ -1,3 +1,4 @@
+import 'package:defiant/pages/page_login.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -6,11 +7,42 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<HomePage> {
+  SlideTransition createTransition(Animation<double> animation, Widget child) {
+    return new SlideTransition(
+      position: new Tween<Offset>(
+        begin: const Offset(1.0, 0.0),
+        end: const Offset(0.0, 0.0),
+      ).animate(animation),
+      child: child,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('HomePage'),
+      ),
+      body: new Center(
+        child: new FlatButton(
+            onPressed: () {
+              Navigator.push<String>(
+                  context,
+                  new PageRouteBuilder(pageBuilder: (BuildContext context,
+                      Animation<double> animation,
+                      Animation<double> secondaryAnimation) {
+                    // 跳转的路由对象
+                    return new LoginPage();
+                  }, transitionsBuilder: (
+                    BuildContext context,
+                    Animation<double> animation,
+                    Animation<double> secondaryAnimation,
+                    Widget child,
+                  ) {
+                    return createTransition(animation, child);
+                  }));
+            },
+            child: Text("Login")),
       ),
     );
   }
---
+++ lib/pages/page_login.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  LoginPageState createState() => new LoginPageState();
+}
+
+class LoginPageState extends State<LoginPage> {
+  GlobalKey key = new GlobalKey<FormState>();
+  TextEditingController _nameController = new TextEditingController();
+  TextEditingController _pwdController = new TextEditingController();
+  bool onclick = false;
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Login'),
+      ),
+      body: new ListView(
+        children: <Widget>[
+          new Center(
+            child: Padding(
+                padding: EdgeInsets.fromLTRB(15.0, 30.0, 15.0, 10.0),
+                child: Form(
+                  child: new Column(
+                    children: <Widget>[
+                      TextFormField(
+                          autofocus: true,
+                          keyboardType: TextInputType.phone,
+                          controller: _nameController,
+                          decoration: InputDecoration(
+                              labelText: "用户名",
+                              hintText: "用户名或邮箱",
+                              icon: Icon(Icons.person),
+                              suffixIcon: new IconButton(
+                                  icon: new Icon(
+                                    Icons.clear,
+                                    color: Colors.black45,
+                                  ),
+                                  onPressed: () {
+                                    _nameController.clear();
+                                  })),
+                          // 校验用户名
+                          validator: (v) {
+                            return v.trim().length > 0 ? null : "用户名不能为空";
+                          }),
+                      TextFormField(
+                          controller: _pwdController,
+                          decoration: InputDecoration(
+                              labelText: "密码",
+                              hintText: "您的登录密码",
+                              icon: Icon(Icons.lock),
+                              suffixIcon: new IconButton(
+                                  icon: new Icon(
+                                    onclick
+                                        ? Icons.remove_red_eye
+                                        : Icons.panorama_fish_eye,
+                                    color: Colors.black45,
+                                  ),
+                                  onPressed: () {
+                                    setState(() {
+                                      onclick = !onclick;
+                                    });
+                                  })),
+                          obscureText: onclick,
+                          //校验密码
+                          validator: (v) {
+                            return v.trim().length > 5 ? null : "密码不能少于6位";
+                          }),
+                      new Padding(
+                        padding: EdgeInsets.only(top: 50.0),
+                        child: Row(
+                          children: <Widget>[
+                            Expanded(
+                                child: RaisedButton(
+                              onPressed: () {
+                                if ((key.currentState as FormState)
+                                    .validate()) {
+                                  print("pass");
+                                }
+                              },
+                              padding: EdgeInsets.all(15.0),
+                              child: Text("Login"),
+                              color: Theme.of(context).primaryColor,
+                              textColor: Colors.white,
+                            ))
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                  key: key,
+                  autovalidate: false,
+                )),
+          )
+        ],
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  @override
+  void didUpdateWidget(LoginPage oldWidget) {
+    // TODO: implement didUpdateWidget
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  void didChangeDependencies() {
+    // TODO: implement didChangeDependencies
+    super.didChangeDependencies();
+  }
+}
